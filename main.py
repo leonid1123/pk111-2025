d3b1a6ff96b065ef55170474a969a9b4f244fe3e
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-03 15:48:27 +0300

    работа с датой

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from tkinter import *
+from datetime import datetime
+from tkcalendar import DateEntry
+"""
+сделать ввод даты, проверку на текущую дату и изменение свежести
+привязать всё к БД
+"""
 
 
 class Fruits:
@@ -13,6 +19,8 @@ class Fruits:
 
 class App:
     def __init__(self):
+        self.today = datetime.today().date()
+        print(self.today)
         self.fruit_list = []
         self.win = Tk()
         self.win.title("Склад")
@@ -20,11 +28,13 @@ class App:
         self.fruit_name_label = Label(text="Название:")
         self.fruit_fresh_label = Label(text="Свежесть:")
         self.fruit_weight_label = Label(text="Вес:")
+        self.fruit_date = Label(text="Дата поставки: ")
         self.name_entry = Entry()
         self.fresh_entry = Entry()
         self.weight_entry = Entry()
         self.btn = Button(text="Добавить")
         self.btn.bind('<Button>', self.add_fruit, add=None)
+        self.date_picker = DateEntry(width=12, background='darkblue', foreground='white', borderwidth=2)
 
         self.fruit_var = Variable(value=self.fruit_list)
         self.fruit_view = Listbox(listvariable=self.fruit_var, width=50)
@@ -33,9 +43,11 @@ class App:
         self.fruit_name_label.grid(row=0, column=0)
         self.fruit_fresh_label.grid(row=1, column=0)
         self.fruit_weight_label.grid(row=2, column=0)
+        self.fruit_date.grid(row=3, column=0)
         self.name_entry.grid(row=0,column=1)
         self.fresh_entry.grid(row=1, column=1)
         self.weight_entry.grid(row=2, column=1)
+        self.date_picker.grid(row=3,column=1)
         self.btn.grid(row=4, column=0, columnspan=2)
 
         self.win.mainloop()
@@ -47,6 +59,8 @@ class App:
         self.fruit_list.append(fruit)
         self.fruit_var.set(self.fruit_list)
 
+        print((self.date_picker.get_date()-self.today).days)
+
         print(self.name_entry.get())
         print(self.fresh_entry.get())
         print(self.weight_entry.get())
@@ -54,7 +68,9 @@ class App:
 
 myApp = App()
 
+
 """
+https://github.com/leonid1123/pk111-2025
 Склад мобильных телефонов.
 класс с 4 параметрами.
 сделать приложение для внесения позиций

dc067a21908631d8ff1016cd8c3ebdb3a1b1f272
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-15 12:29:08 +0300

    update

--- main.py
+++ main.py
@@ -1,52 +1,59 @@
-import random
-#переложить в интерфейс
-class Player:
+from tkinter import *
+import pymysql.cursors
+
+
+class App:
     def __init__(self):
-        self.HP = 10
-        self.defence = 0
-        self.atk = 0
+        self.win = Tk()
+        self.win.geometry("300x300")
+        self.win.title("Мессенджер")
+        self.messages = []
+        self.messages_var = Variable(value=self.messages)
+        self.msg_view = Listbox(width=45, listvariable=self.messages_var)
+        self.msg_entry = Entry(width=30)
+        self.send_btn = Button(text="Отправить", command=self.send_message)
+        self.get_btn = Button(text="Получить", command=self.get_messages)
 
-    def change_HP(self):
-        self.HP -= 1
+        self.win.columnconfigure(0, weight=5)
+        self.win.columnconfigure(1, weight=1)
+        self.msg_view.grid(row=0, column=0, columnspan=2, sticky=EW)
+        self.msg_entry.grid(row=1, column=0, sticky=EW)
+        self.send_btn.grid(row=1, column=1)
+        self.get_btn.grid(row=2, column=0, columnspan=2, sticky=EW)
 
-    def change_def(self, _pos):
-        self.defence = _pos
+        self.cnx = pymysql.connect(
+            host="192.168.1.61",
+            user="messenger",
+            password="1234",
+            database="messenger"
+        )
+        self.cur = self.cnx.cursor()
+        self.get_messages()
+        self.update_messages()
+        self.win.mainloop()
 
-    def change_atk(self, _pos):
-        self.atk = _pos
+    def send_message(self):
+        msg = self.msg_entry.get()
+        if msg:
+            sql = "INSERT INTO msg(message) VALUES (%s)"
+            params = (msg,)
+            self.cur.execute(sql, params)
+            self.cnx.commit()
+            self.msg_entry.delete(0, END)
+            self.get_messages()
+
+    def get_messages(self):
+        self.cur.execute("SELECT message FROM msg")
+        ans = self.cur.fetchall()
+        new_messages = [item[0] for item in ans]
+        if new_messages != self.messages:
+            self.messages = new_messages
+            self.messages_var.set(self.messages)
+            self.msg_view.yview_moveto(1)
+
+    def update_messages(self):
+        self.get_messages()
+        self.win.after(5000, self.update_messages)
 
 
-class Goblin:
-    def __init__(self):
-        self.HP = 10
-        self.defence = 0
-        self.atk = 0
-
-    def change_HP(self):
-        self.HP -= 1
-
-    def change_def(self):
-        self.defence = random.randint(1,3)
-
-    def change_atk(self):
-        self.atk = random.randint(1,3)
-
-player = Player()
-goblin = Goblin()
-while True:
-    player_atk_pos = input("позиция атаки(1,2,3)")
-    player_atk_pos = int(player_atk_pos)
-    player_def_pos = input("позиция защиты(1,2,3)")
-    player_def_pos = int(player_def_pos)
-    player.change_atk(player_atk_pos)
-    player.change_def(player_def_pos)
-    goblin.change_def()
-    goblin.change_atk()
-    if goblin.defence != player.atk:
-        goblin.change_HP()
-    if player.defence != goblin.atk:
-        player.change_HP()
-    print(f"Player HP {player.HP}, Def:{player.defence},Atk:{player.atk}")
-    print(f"Goblin HP {goblin.HP}, Def:{goblin.defence},Atk:{goblin.atk}")
-    if player.HP<1 or goblin.HP<1:
-        break
+app = App()

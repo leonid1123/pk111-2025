1ad3e590fdbee281291c927b4f0edba1efd42526
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-29 13:44:32 +0300

    update

--- login.py
+++ login.py
@@ -4,8 +4,9 @@ import pymysql
 
 class App:
     def __init__(self):
-        my_style = {'font':('Helvetica', 18)}
-        paddings = { 'padx': 5, 'pady': 5}
+        my_style = {'font': ('Helvetica', 18)}
+        paddings = {'padx': 5, 'pady': 5}
+        self.reg_win = None
         self.win = Tk()
         self.win.geometry("600x200")
         self.win.title("Авторизация")
@@ -14,11 +15,15 @@ class App:
         self.login_entry = Entry(**my_style)
         self.pass_entry = Entry(**my_style)
         btn = Button(text="Вход", **my_style, command=self.check_password)
+        reg_btn = Button(text="Регистрация",
+                         **my_style,
+                         command=self.register_user)
         login_text.grid(row=0, column=0, **paddings)
         pass_text.grid(row=1, column=0, **paddings)
         self.login_entry.grid(row=0, column=1, **paddings)
         self.pass_entry.grid(row=1, column=1, **paddings)
-        btn.grid(row=3, column=0, columnspan=2, **paddings)
+        btn.grid(row=2, column=0, **paddings)
+        reg_btn.grid(row=2, column=1, **paddings)
         self.db_connect()
         self.win.mainloop()
 
@@ -36,12 +41,50 @@ class App:
                 FROM user
                  WHERE login = %s"""
         params = (login,)
-        self.cur.execute(sql,params)
+        self.cur.execute(sql, params)
         ans = self.cur.fetchall()
         tmp = ans[0]
         db_pass = tmp[0]
         print(db_pass)
+        if db_pass == password:
+            print("ok")
+        else:
+            print("не ok")
 
+    def register_user(self):
+        if self.reg_win is None:
+            self.reg_win = Tk()
+            self.reg_win.title("Регистрация")
+            self.reg_win.geometry("500x300")
+            lbl_names = ["Логин", "Пароль", "Повторите пароль"]
+            for row, item in enumerate(lbl_names, start=0):
+                Label(self.reg_win, text=item).grid(row=row, column=0)
+            self.new_login_entry = Entry(self.reg_win)
+            self.new_pass_entry1 = Entry(self.reg_win)
+            self.new_pass_entry2 = Entry(self.reg_win)
+            self.new_login_entry.grid(row=0, column=1)
+            self.new_pass_entry1.grid(row=1, column=1)
+            self.new_pass_entry2.grid(row=2, column=1)
+            Button(self.reg_win, text="Регистрация",
+                   command=self.reg_user).grid(row=3,
+                                               column=0,
+                                               columnspan=2)
+            self.reg_win.mainloop()
+
+    def reg_user(self):
+        new_login = self.new_login_entry.get()
+        new_pass1 = self.new_pass_entry1.get()
+        new_pass2 = self.new_pass_entry2.get()
+        if new_pass1 == new_pass2:
+            sql = """SELECT id FROM users
+            WHERE login=%s"""
+            params = (new_login,)
+            self.cur.execute(sql,params)
+            ans = self.cur.fetchone()
+            if ans:
+                print("такое имя занято")
+        else:
+            print("Пароли не совпадают!")
 
 
 app = App()
--- main.py
+++ main.py
@@ -1,59 +1,68 @@
+#06/05 и 13/05 контрольные работы
+
 from tkinter import *
-import pymysql.cursors
+import pymysql
 
 
 class App:
     def __init__(self):
-        self.win = Tk()
-        self.win.geometry("300x300")
-        self.win.title("Мессенджер")
-        self.messages = []
-        self.messages_var = Variable(value=self.messages)
-        self.msg_view = Listbox(width=45, listvariable=self.messages_var)
-        self.msg_entry = Entry(width=30)
-        self.send_btn = Button(text="Отправить", command=self.send_message)
-        self.get_btn = Button(text="Получить", command=self.get_messages)
-
-        self.win.columnconfigure(0, weight=5)
-        self.win.columnconfigure(1, weight=1)
-        self.msg_view.grid(row=0, column=0, columnspan=2, sticky=EW)
-        self.msg_entry.grid(row=1, column=0, sticky=EW)
-        self.send_btn.grid(row=1, column=1)
-        self.get_btn.grid(row=2, column=0, columnspan=2, sticky=EW)
-
-        self.cnx = pymysql.connect(
-            host="192.168.1.61",
-            user="messenger",
-            password="1234",
-            database="messenger"
-        )
+        self.cnx = pymysql.connect(host="localhost",
+                                   user="pk111",
+                                   password="1234",
+                                   database="pk111")
         self.cur = self.cnx.cursor()
-        self.get_messages()
-        self.update_messages()
-        self.win.mainloop()
+        win = Tk()
+        win.title("Повторение")
+        win.geometry("350x300")
+        title_lbl = Label(text="Процессоры")
+        name_lbl = Label(text="Название")
+        man_lbl = Label(text="Производитель")
+        fr_lbl = Label(text="Частота")
+        sok_lbl = Label(text="Сокет")
+        self.name_entry = Entry()
+        self.man_entry = Entry()
+        self.fr_entry = Entry()
+        self.sok_entry = Entry()
+        add_btn = Button(text="Добавить", height=6, command=self.get_db_info)
+        self.my_list = []
+        self.my_list_var = Variable(value=self.my_list)
+        my_view = Listbox(listvariable=self.my_list_var, width=55)
+        style={"padx":5,"pady":5, "sticky":"W"}
+        my_view.grid(row=0, column=0, columnspan=3, **style)
+        name_lbl.grid(row=1, column=0, **style)
+        man_lbl.grid(row=2, column=0, **style)
+        fr_lbl.grid(row=3, column=0, **style)
+        sok_lbl.grid(row=4, column=0, **style)
+        self.name_entry.grid(row=1, column=1, **style)
+        self.man_entry.grid(row=2, column=1, **style)
+        self.fr_entry.grid(row=3, column=1, **style)
+        self.sok_entry.grid(row=4, column=1, **style)
+        add_btn.grid(row=1, column=2, rowspan=4, **style)
+        win.mainloop()
 
-    def send_message(self):
-        msg = self.msg_entry.get()
-        if msg:
-            sql = "INSERT INTO msg(message) VALUES (%s)"
-            params = (msg,)
-            self.cur.execute(sql, params)
-            self.cnx.commit()
-            self.msg_entry.delete(0, END)
-            self.get_messages()
+    def add_item(self):
+        name = self.name_entry.get()
+        man = self.man_entry.get()
+        fr = self.fr_entry.get()
+        sok = self.sok_entry.get()
+        if name and man and fr and sok:
+            tmp_str = f"Название:{name}, производитель:{man}, частота:{fr}"
+            self.my_list.append(tmp_str)
+            self.my_list_var.set(self.my_list)
 
-    def get_messages(self):
-        self.cur.execute("SELECT message FROM msg")
+    def get_db_info(self):
+        self.cur.execute("SELECT * FROM proc")
         ans = self.cur.fetchall()
-        new_messages = [item[0] for item in ans]
-        if new_messages != self.messages:
-            self.messages = new_messages
-            self.messages_var.set(self.messages)
-            self.msg_view.yview_moveto(1)
+        print(ans)
+        for item in ans:
+            print(item)
+            print(f"name:{item[1]},manuf:{item[2]},freq:{item[3]}")
 
-    def update_messages(self):
-        self.get_messages()
-        self.win.after(1000, self.update_messages)
+
+
+
+
 
 
 app = App()
+
